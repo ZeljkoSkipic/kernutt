7709c1e83c497baded0a9eceff0ef53912511891
ZeljkoSkipic <zeljko.skipic@gmail.com>
2023-08-02 12:58:05 +0200

    Post Author

--- single.php
+++ single.php
@@ -4,7 +4,25 @@
 	<div class="container clearfix">
 		<div id="left-area">
 			<h1><?php the_title(); ?></h1>
-			<span class="ks_sp_author">By <?php echo get_the_author_meta('display_name'); ?></span>
+
+			<?php
+			$contributors = get_field('contributors');
+			if( $contributors ): ?>
+				<span>By</span>
+				<?php foreach( $contributors as $contributor ):
+					$permalink = get_permalink( $contributor->ID );
+					$title = get_the_title( $contributor->ID );
+					$member_job_title = get_field( 'member_job_title', $contributor->ID );
+					$terms = get_the_terms( $contributor->ID, 'roles' );
+					?>
+						<span class="ks_sp_author"><a href="<?php echo esc_url( $permalink ); ?>"><?php echo esc_html( $title ); ?></a>,
+						<?php echo esc_html( $member_job_title ); ?>
+						<?php foreach ( $terms as $term ) {
+							echo $term->name;
+						} ?></span>
+				<?php endforeach; ?>
+			<?php endif; ?>
+
 			<div class="kn_sp_content">
 				<?php the_content(); ?>
 			</div>
