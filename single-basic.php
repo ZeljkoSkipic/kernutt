2f3aca57d5b13719fbdfda1f92636230338ec494
zeljkoskipic <zeljko.skipic@gmail.com>
2023-08-10 11:54:26 +0200

    Post BTN

--- single-basic.php
+++ single-basic.php
@@ -35,6 +35,17 @@ get_header(); ?>
 			<div class="kn_sp_content">
 				<?php the_content(); ?>
 			</div>
+			<?php
+			$pdf = get_field('pdf');
+			if( $pdf ):
+				$pdf_url = $pdf['url'];
+				$pdf_title = $pdf['title'];
+				$pdf_target = $pdf['target'] ? $pdf['target'] : '_self';
+				?>
+				<div class="post_btn_wrap">
+					<a class="post_pdf" href="<?php echo esc_url( $pdf_url ); ?>" target="<?php echo esc_attr( $pdf_target ); ?>"><?php echo esc_html( $pdf_title ); ?></a>
+				</div>
+			<?php endif; ?>
 		</div>
 
 		<?php get_sidebar(); ?>
